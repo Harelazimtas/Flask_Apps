c65ad3b20b6f9419561c1d8656e286043b148bd9
Harel Azim tas <Harel.Azim.tas@s.afeka.ac.il>
2020-04-28 23:06:36 +0300

    added FLASK_APPS

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 import json
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app=Flask(__name__)
 
 ENV='dev'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eunamewfsidzet:3dc1416abf5e68aca5f4dec3695b6f57153fa05655c027fabfe186c3b0b6cf7b@ec2-50-17-21-170.compute-1.amazonaws.com:5432/d5ukkd9etal4nu'
 
+db = SQLAlchemy(app)
+db.create_all()
+
+class users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(200))
+    name = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    create_time = db.Column(db.DateTime,default=datetime.now)
+    status = db.Column(db.Integer)
+
+    def __init__(self, id, nickname, name, password,create_time,status):
+        self.id = id
+        self.nickname = nickname
+        self.name = name
+        self.password = password
+        self.create_time = create_time
+        self.status = status
 
 @app.route('/')
 def index():
@@ -38,6 +60,9 @@ def show_all():
      #   print(Id," ",name)
       #  return render_template('index.html')
 
+
+
 if __name__== "__main__":
+    db.create_all()
     app.debug=True
     app.run() 
