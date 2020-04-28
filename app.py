85a029ea27e5a4b1166a903322754b147253d6a9
Harel Azim tas <Harel.Azim.tas@s.afeka.ac.il>
2020-04-28 16:48:31 +0300

    added Flask_APPS

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
+ENV='dev'
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/submit',methods=['POST'])
 def submit():
